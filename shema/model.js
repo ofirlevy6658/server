76d884fa30c97a64b9938edceaaa30c7a9adcb0a
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-04-27 13:46:48 +0300

    heruko

---
+++ shema/model.js
@@ -0,0 +1,30 @@
+const dynamo = require("dynamodb"); //need to download
+// const Joi = require('joi');
+dynamo.AWS.config.loadFromPath("credentials.json"); //need the path from Tanya
+dynamo.AWS.config.update({
+	accessKeyId: "AKIAX3XFV4XXX6R2XBO3",
+	secretAccessKey: "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",
+	region: "REGION",
+}); //need the region from Tanya
+
+const Item = dynamo.define("Item", {
+	hashKey: "email",
+	// add the timestamp attributes (updatedAt, createdAt)
+	timestamps: true,
+	schema: {
+		url: dynamo.types.string(),
+		title: dynamo.types.string(),
+		brand: dynamo.types.string(),
+		price: dynamo.types.number(),
+		isTransalated: dynamo.types.boolean(),
+		titleRussian: dynamo.types.string(),
+		titleArabic: dynamo.types.string(),
+		titleFrench: dynamo.types.string(),
+		description: dynamo.types.string(),
+		descritionRussian: dynamo.types.string(),
+		descritionArabic: dynamo.types.string(),
+		descritionFrench: dynamo.types.string(),
+	},
+});
+
+module.exports = ItemModel;
