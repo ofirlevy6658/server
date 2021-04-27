24302a4e98fa285b099c5bec42eb43b11e047741
Sergey Aronov <AronovSergeyy@gmail.com>
2021-04-27 23:13:25 +0300

    Update dynamo.js

--- shema/dynamo.js
+++ shema/dynamo.js
@@ -13,10 +13,9 @@ const TABLE_NAME = "test";
 
 const getProducts = async () => {
 	const params = {
-		tableName: TABLE_NAME,
+		TableName: TABLE_NAME,
 	};
 	const products = await dynamoClient.scan(params).promise();
-	console.log(products);
 	return products;
 };
 
@@ -28,18 +27,29 @@ const addOrUpdateProduct = async (product) => {
 	return await dynamoClient.put(params).promise();
 };
 
-const product = {
-	url: "https://www.azrieli.com/o/bbb71506-ff57-490d-b530-9daaa1c0141e",
-	title: "בושם לאישה Prada Infusion Diris E.D.P 100 ﻿",
-	price: "339",
-	isTransalated: false,
-	titleRussian: undefined,
-	titleFrance: undefined,
-	titleArbic: undefined,
-	numOfPayments: "ניתן לפרוס עד 6  תשלומים ללא ריבית",
-	numOfPaymentsRussian: undefined,
-	numOfPaymentsFrance: undefined,
-	numOfPaymentsArbic: undefined,
+const getProductById = async (id) => {
+	const params = {
+		TableName: TABLE_NAME,
+		Key: {
+			id,
+		},
+	};
+	return await dynamoClient.get(params).promise();
 };
 
-addOrUpdateProduct(product);
+const deleteProduct = async (id) => {
+	const params = {
+		TableName: TABLE_NAME,
+		Key: {
+			id,
+		},
+	};
+	return await dynamoClient.delete(params).promise();
+};
+
+module.exports = {
+	getProducts,
+	addOrUpdateProduct,
+	getProductById,
+	deleteProduct,
+};
