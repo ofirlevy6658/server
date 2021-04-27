48d0b140fe41f36109c8b95fd194e1350dd3697c
Sergey <aronovsergeyy@gmail.com>
2021-04-27 13:08:16 +0300

    add scraper

---
+++ scraper/scraper.js
@@ -0,0 +1,41 @@
+const puppeteer = require("puppeteer");
+
+const scraper = async (url) => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto(url);
+
+	const title = await page.evaluate(
+		() => document.querySelector("h2.deal-title").innerHTML
+	);
+
+	const price = await page.evaluate(
+		() => document.querySelector("#DealPrice").innerHTML
+	);
+
+	const brend = await page.evaluate(
+		() => document.querySelector("#DealPrice").innerHTML
+	);
+
+	await browser.close();
+
+	return {
+		url,
+		title,
+		price,
+		titleRussian: undefined,
+		titleFrance: undefined,
+		titleArbic: undefined,
+		descriptionRussian: undefined,
+		descriptionFrance: undefined,
+		descriptionArbic: undefined,
+	};
+};
+
+(async () => {
+	console.log(
+		await scraper(
+			"https://www.azrieli.com/o/125621e9-4da0-47c9-8992-6e0fdc1c2169"
+		)
+	);
+})();
